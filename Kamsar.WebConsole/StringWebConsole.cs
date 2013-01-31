bb8d7e79a26152367e230d05e01c5a721bb40516
Kam Figy <kamsar@gmail.com>
2013-01-31 13:36:59 -0800

    Removed null console; replaced with string console
    This lets you read the console output at least.

---
+++ Kamsar.WebConsole/StringWebConsole.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+using System.Web;
+
+namespace Kamsar.WebConsole
+{
+	/// <summary>
+	/// Variant of the WebConsole that saves the lines to a StringBuilder for programmatic capture
+	/// 
+	/// Progress reports are not captured.
+	/// </summary>
+	public class StringWebConsole : WebConsole
+	{
+		StringBuilder _output = new StringBuilder();
+		public StringWebConsole() : base(HttpContext.Current.Response)
+		{
+		}
+
+		public override void Write(string statusMessage, MessageType type, params object[] formatParameters)
+		{
+			var line = new StringBuilder();
+
+			line.AppendFormat("{0}: ", type);
+
+			if (formatParameters.Length > 0)
+				line.AppendFormat(statusMessage, formatParameters);
+			else
+				line.Append(statusMessage);
+
+			_output.Append(HttpUtility.HtmlEncode(line.ToString()));
+		}
+
+		public override void WriteLine(string statusMessage, MessageType type, params object[] formatParameters)
+		{
+			Write(statusMessage + "\n", type, formatParameters);
+		}
+
+		public override void SetProgress(int percent)
+		{
+			// do nothing
+		}
+
+		public override void WriteScript(string script)
+		{
+			// do nothing
+		}
+
+		public override void SetProgressStatus(string statusMessage, params object[] formatParameters)
+		{
+			// do nothing
+		}
+
+		public string Output { get { return _output.ToString(); } }
+	}
+}
