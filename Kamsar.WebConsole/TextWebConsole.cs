3a06b786616e9e85b180a18455759960f3f59e51
Kam Figy <kamsar@gmail.com>
2017-02-20 23:28:17 -0800

    Normalize TextWebConsole with Html5WebConsole's UX

--- Kamsar.WebConsole/TextWebConsole.cs
+++ Kamsar.WebConsole/TextWebConsole.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 using System.Web;
 
 namespace Kamsar.WebConsole
@@ -8,14 +9,32 @@ namespace Kamsar.WebConsole
 	/// Useful when capturing text logs of a console (eg for non-web-emission contexts)
 	/// 
 	/// Progress reports are not captured.
+	/// 
+	/// Requires disposal UNLESS using Render(Action) method
 	/// </summary>
 	public class TextWebConsole : WebConsole
 	{
 		private readonly HttpResponseBase _response;
 
-		public TextWebConsole(HttpResponseBase response, bool forceBuffer = true) : base(response, forceBuffer)
+		public TextWebConsole(HttpResponseBase response, bool forceBuffer = true, bool setContentType = true) : base(response, forceBuffer)
 		{
+			if (setContentType)
+			{
+				response.ContentType = "text/plain";
+			}
+
 			_response = response;
+		}
+
+		public override void Render()
+		{
+			throw new NotImplementedException("Use the overload with the process action to avoid needing to dispose this.");
+		}
+
+		public virtual void Render(Action<IProgressStatus> processAction)
+		{
+			processAction(this);
+			Dispose(true);
 		}
 
 		public override void Write(string statusMessage, MessageType type, params object[] formatParameters)
@@ -55,7 +74,7 @@ namespace Kamsar.WebConsole
 			// do nothing
 		}
 
-		public bool HasErrors { get; private set; }
-		public bool HasWarnings { get; private set; }
+		public virtual bool HasErrors { get; private set; }
+		public virtual bool HasWarnings { get; private set; }
 	}
 }
